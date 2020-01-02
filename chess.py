fbee43626e7b4ccaab532dfeb9dc84496044ce90
Priyankazoya <55803487+Priyankazoya@users.noreply.github.com>
2020-01-02 14:45:47 +0530

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,15 @@
+class Vehical():
+    def __init__(self)
+        self.color = input("enter color of vehical")
+        self.number = int(input("enter car number "))
+        self.wheels = int(input("how many wheels of the car number ")d)
+    def print_name(self):
+        print(self.name.capitalize())
+    def print_rollno(self):
+        print(self.rollno)
+    def print_subjects(self):
+        print(self.subjects)
+        
+        
+student = FirstClass('amith',2,6)
+student.print_name()
